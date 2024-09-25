a29016ece4f244c4ca64bd1f5920314aef2db4c2
valpas312 <valenpalmas2005@gmail.com>
2024-09-24 22:34:52 -0300

    fuentes tipograficas con shadn

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,20 @@
 import { Medal } from "lucide-react";
+import localFont from "next/font/local";
 
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const headingFont = localFont({
+  src: "../fonts/GeistMonoVF.woff"
+});
 
 export default function MarketingPage() {
   return (
     <div className="flex items-center justify-center flex-col">
-      <div className="flex items-center justify-center flex-col">
+      <div className={cn("flex items-center justify-center flex-col",
+        headingFont.className
+      )}>
         <div className="mb-4 flex items-center border shadow-md p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
           <Medal className="h-6 w-6 mr-2" />
           No 1 task managment
@@ -18,11 +27,13 @@ export default function MarketingPage() {
         </div>
       </div>
       <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md::max-w-2xl text-center mx-auto">
-        Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique - accomplish it all with Taskify.
+        Collaborate, manage projects, and reach new productivity peaks. From
+        high rises to the home office, the way your team works is unique -
+        accomplish it all with Taskify.
       </div>
-      <Button>
-        Get Started
+      <Button className="mt-6" size="lg" asChild>
+        <Link href="/signup">Get Taskify for free</Link>
       </Button>
-    </div>      
+    </div>
   );
 }
